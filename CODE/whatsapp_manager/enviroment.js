e9a04b0954368ed7adaa18d8e5a449d5deb51dcd
Djalma Mariano <outs_djalma.oliveira@tigo.com.pa>
2025-07-16 09:50:37 -0300

    fix rabbit mq url

--- CODE/whatsapp_manager/enviroment.js
+++ CODE/whatsapp_manager/enviroment.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 8080;
 const SESSION_ID = process.env.SESSION_ID || `session-123`;
 const SESSION_DIR = process.env.SESSION_DIR || './data';
 const DEBUG = process.env.DEBUG === 'true';
-const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://zapbless:zapbless@k8s-dev-sat.zapbless.com.br';
+const RABBITMQ_URL = process.env.RABBITMQ_URL || 'amqp://zapbless:zapbless@rabbitmq-v1-services.argocd.svc.cluster.local:5672';
 const RABBITMQ_OUTBOUND_QUEUE = process.env.RABBITMQ_OUTBOUND_QUEUE || 'zapbless.outbound';
 const RABBITMQ_INBOUND_QUEUE = process.env.RABBITMQ_INBOUND_QUEUE || 'zapbless.inbound';
 const RABBITMQ_DISCONNECTED_SESSIONS_QUEUE = process.env.RABBITMQ_DISCONNECTED_SESSIONS_QUEUE || 'zapbless.disconnected.sessions';
